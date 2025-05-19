8808d85537239262d9dee4c3d97eaa359b1325a4
rehan6025 <rehanthakur6025@gmail.com>
2025-05-19 23:21:19 +0530

    prettier not working

--- app/page.js
+++ app/page.js
@@ -1,5 +1,10 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <Button variant="destructive">Hello Rehan</Button>;
+  return 
+    <div>
+
+          <Button variant="destructive">Hello Rehan</Button>;
+
+    </div>
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -5,18 +5,34 @@ import {
   SignUpButton,
   UserButton,
 } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { Button } from "./ui/button";
 
 const Header = () => {
   return (
-    <div>
+    <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+
+      <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
+        <Link href="/">
+          <Image src={"/logo.png"} alt="Welth Logo"
+            width={200}
+            height={60}
+            className="h-12 w-auto object-contain"
+            />
+        </Link>
+
       <SignedOut>
-        <SignInButton />
-        <SignUpButton />
+        <SignInButton forceRedirectUrl="/dashboard">
+          <Button variant="outline">Sign in </Button>
+        </SignInButton>
+             <SignUpButton />
       </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
+      </nav>
     </div>
   );
 };
